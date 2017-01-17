1194d5086fb35fcd8a03af9bca4e1bd5f23aa5a8
julioppgca <julio.ppgca@gmail.com>
2017-01-16 23:35:40 -0200

    Updated.

--- ptgmed_inc/Network_config.c
+++ ptgmed_inc/Network_config.c
@@ -82,7 +82,7 @@ Void tcpWorker(UArg arg0, UArg arg1)
         }
 
         char id[8];
-        sprintf(id,"%X",Outlet_1.id);
+        sprintf(id,"%X%X%X%X%X%X%X%X",Outlet_1.id[7],Outlet_1.id[6],Outlet_1.id[5],Outlet_1.id[4],Outlet_1.id[3],Outlet_1.id[2],Outlet_1.id[1],Outlet_1.id[0]);
         send(clientfd, id, sizeof(id), 0);
 
     }
